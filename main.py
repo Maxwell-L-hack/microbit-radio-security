3867223fb59d22839663b25fea04227e5bab2909
Maxwell Lenhoff <maxwelll@hackclub.app>
2026-06-23 10:55:26 -0500

    Made it continuously check for the ready message

--- main.py
+++ main.py
@@ -18,11 +18,21 @@ def initiate_mode():
 
     pre_shared_key = 168856323
 
-    radio.send(str(random_channel))
+    microbit.sleep(1000)
+    while True:
+        confirmation = radio.receive()
+        if confirmation != 'ready':
+            microbit.display.scroll('W')
+        elif confirmation == 'ready':
+            microbit.display.scroll('Confirmation received')
+            radio.config(channel=random_channel, power=7)
 
-    radio.config(channel=random_channel, power=7)
 
-def recieve_mode():
+
+def receive_mode():
+
+    microbit.display.scroll('Receiving')
+
     starter_channel = 6
 
     radio.reset()
@@ -31,11 +41,15 @@ def recieve_mode():
     pre_shared_key = 168856323
     new_channel = radio.receive()
 
-    if new_channel is not None:
+    while new_channel is not None:
+        radio.send('ready')
         new_channel_int = int(new_channel)
         radio.config(channel=new_channel_int, power=7)
         microbit.display.scroll(new_channel_int)
+        microbit.sleep(1000)
 
 while True:
     if microbit.pin_logo.is_touched():
         initiate_mode()
+    elif microbit.button_a.was_pressed():
+        receive_mode()
