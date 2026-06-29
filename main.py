75cfebad04d56b09a32c9fe1954bed634ec62a7a
Maxwell-L-hack <papalenhoff@gmail.com>
2026-06-29 16:44:23 -0500

    Removed commented code for reimplementing

--- main.py
+++ main.py
@@ -18,17 +18,6 @@ def initiate_mode():
 
     pre_shared_key = 168856323
 
-    #while True:
-        #random_channel = random.randint(0, 83)
-        #if new_channel != random_channel:
-        #    new_channel = random_channel
-        #    return new_channel
-        #else:
-        #    random_channel = random.randint(0, 83)
-        #    return random_channel
-
-
-
 def receive_mode():
 
     microbit.display.scroll('Receiving')
@@ -43,10 +32,6 @@ def receive_mode():
 
     while True:
         radio.send('ready')
-        #new_channel_int = int(new_channel)
-        #radio.config(channel=new_channel_int, power=7)
-        #microbit.display.scroll(new_channel_int)
-        #microbit.sleep(1000)
 
 def waiting_animation():
     while True:
@@ -74,15 +59,6 @@ def check_if_ready():
     while True:
         confirmation = radio.receive()
         microbit.display.scroll(str(confirmation))
-
-    #while confirmation != 'ready':
-    #    confirmation = radio.receive()
-    #    waiting_animation()
-#
-    #is_ready = 1
-    #
-    #if is_ready == 1:
-    #    microbit.display.scroll('Confirmation received')
 
 while True:
     if microbit.pin_logo.is_touched():
